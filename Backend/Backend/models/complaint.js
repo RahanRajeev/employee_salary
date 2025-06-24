015ef508f4f0a7d31fc8f407d998d74b9afd3733
RahanRajeev <rahanrajeev003@gmail.com>
2025-06-24 15:37:09 +0530

    Remove nested Backend and frontendproject folders from tracking

--- Backend/Backend/models/complaint.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const complaintschema=new mongoose.Schema({
-    date:String,
-    complaint:String,
-    reply:String,
-    userid:{type:mongoose.Schema.Types.ObjectId,ref:'user'}
-})
-const complaint=mongoose.model('complaint',complaintschema)
-module.exports=complaint
--- Backend/Backend/models/review.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const reviewschema=new mongoose.Schema({
-    rating:String,
-    review:String,
-    date:String,
-    userid:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
-})
-const review=mongoose.model('review',reviewschema)
-module.exports=review
